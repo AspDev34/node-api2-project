f4a0ad67992310ef7f92bd88fd17e344b9d9f085
Ike Otu <ikeotu@msn.com>
2020-04-08 13:44:46 -0700

    get request by id completed

--- blog resources/blogRouter.js
+++ blog resources/blogRouter.js
@@ -15,3 +15,17 @@ router.get('/', (req, res) => {
         res.status(500).json({message: 'error retrieveing data'})
     })
 });
+
+// 	Returns the post object with the specified id.
+router.get('/:id', (req, res) => {
+    !req.params.id ? res.status(404).json({message: 'no such post exists'}): blog.findById(req.params.id)
+
+    .then(blg => {
+        console.log('blg', blg)
+
+        res.status(201).json(blg);
+    })
+    .catch(err => {
+        res.status(500).json({message: 'unable to retrieve data'})
+    })
+});
